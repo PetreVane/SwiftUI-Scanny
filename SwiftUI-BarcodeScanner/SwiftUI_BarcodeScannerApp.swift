f3d77c76aa9f787a73eb7b5ba04ac7d7f9ab2495
Petre Vane <petre.vane@gmail.com>
2020-11-18 18:18:56 +0100

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Petre Vane on 18/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
